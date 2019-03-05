34aee5d1032d954df1bbc135b3a009af414de2bb
WhereIsMyRum <p.polcik1@gmail.com>
2019-03-05 18:31:18 +0100

    Working, finished mancala

--- mancala.py
+++ mancala.py
@@ -21,6 +21,7 @@ def disableButtons():
         if(smallFieldsArray[i + 6*whoseTurn]['text'] != '0'):
             smallFieldsArray[i + 6*whoseTurn].config(state='normal')
 
+# check if the game is over. If it is, transfer the remaining pebbles to the correct player
 def checkIfEnd():
     counter = 0
     for i in range(0,6):
@@ -29,7 +30,7 @@ def checkIfEnd():
         else:
             counter = counter + 1
     if (counter == 6):
-        print(basesArray[1]['text'])
+        #print(basesArray[1]['text']) # print the base value before adding the remaining pebbles
         for i in range(6, 12):
             basesArray[1]['text'] = str(int(basesArray[1]['text']) + int(smallFieldsArray[i]['text']))
     else:
@@ -40,13 +41,14 @@ def checkIfEnd():
             else:
                 counter = counter + 1
         if (counter == 6):
-            print(basesArray[0]['text'])
+            #print(basesArray[0]['text']) # print the base value before adding the remaining pebbles
             for i in range(0, 6):
                 basesArray[0]['text'] = str(int(basesArray[0]['text']) + int(smallFieldsArray[i]['text']))
     if (counter == 6):
         for i in range(0,12):
             smallFieldsArray[i].config(state='disabled', text='0')
-    
+
+# function for checking if something hasnt been fucked up
 def checkSum():
     suma = 0
     for i in range(0, 12):
@@ -55,67 +57,105 @@ def checkSum():
         suma = suma + int(basesArray[i]['text'])
     print(suma)
 
+def increaseButtonValue(button):
+    temp_value = int(button['text'])
+    button.config(text = str(temp_value+1))
+
+    # if the field the last pebble goes into is empty, and it belongs to the player
+    # that currently has his turn, check if the opposite field is not empty
+    # if it is not empty, then steal the pebbles
+def checkForEmptyField(button, buttonIndex):
+    temp_value = int(button['text'])
+    oppositeFieldIndex = 5 + 1 + (5 - buttonIndex)
+    temp_opposite_value = int(smallFieldsArray[oppositeFieldIndex]['text'])
+    if (temp_value == 0 and temp_opposite_value != 0):
+        for  i in range(0,  temp_opposite_value +1 ):
+            increaseButtonValue(basesArray[whoseTurn])
+        smallFieldsArray[oppositeFieldIndex].config(text = "0")
+        #print('last was zero and opposite not empty! Stealing pebbles from ', oppositeFieldIndex)
+    else:
+        increaseButtonValue(button)
+
 
 def buttonClick(button, buttonID):
+
     global whoseTurn
     value = int(button['text'])
     button.config(text="0")
     startingIndex = int(buttonID)+1
     i = int(buttonID)+1
+
     while (i < int(buttonID)+value+1):
+        # check if the points need to be inserted into left base
         if(startingIndex == 6 and whoseTurn == 0):
-            temp_value = int(basesArray[0]['text'])
-            basesArray[0].config(text = str(temp_value+1))
+            increaseButtonValue(basesArray[0])
+            # check if the point inserted into the base was the last pebble.
+            # if so, give the same  player another turn (else instruction)
             if (i+1 < int(buttonID)+value+1):
-                temp_value = int(smallFieldsArray[startingIndex]['text'])
-                smallFieldsArray[startingIndex].config(text = str(temp_value+1))
+                increaseButtonValue(smallFieldsArray[startingIndex])
+            else:
+                whoseTurn = -1 * whoseTurn + 1
             startingIndex = startingIndex + 1
             i = i + 2
+        # check if the points need to be inserted into rigth base
         elif(startingIndex == 12 and whoseTurn == 1):
-            temp_value = int(basesArray[1]['text'])
-            basesArray[1].config(text = str(temp_value+1))
+            increaseButtonValue(basesArray[1])
+            # check if the point inserted into the base was the last pebble.
+            # if so, give the same  player another turn (else instruction)
             if (i+1 < int(buttonID)+value+1):
-                temp_value = int(smallFieldsArray[0]['text'])
-                smallFieldsArray[0].config(text = str(temp_value+1))
+                increaseButtonValue(smallFieldsArray[0])
+            else:
+                whoseTurn = -1 * whoseTurn + 1
             i = i+2
             startingIndex = 1
+        # reset counting at the right side
         elif(startingIndex == 12):
             startingIndex = 0
+
         else:
-            temp_value = int(smallFieldsArray[startingIndex]['text'])
-            smallFieldsArray[startingIndex].config(text = str(temp_value+1))
+            if( i+1 == int(buttonID)+value+1 and (startingIndex < 6 and whoseTurn == 0 or startingIndex > 5 and whoseTurn == 1)):
+                    checkForEmptyField(smallFieldsArray[startingIndex], startingIndex)
+
+            else:
+                increaseButtonValue(smallFieldsArray[startingIndex])
             startingIndex = startingIndex + 1
             i = i+1
+
     whoseTurn = -1 * whoseTurn + 1
     disableButtons()
-    checkSum()
+    #checkSum()
     checkIfEnd()
     return 
 
-smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="red", command=lambda: buttonClick(smallFieldsArray[0], 0)))
-smallFieldsArray[0].grid(row=0, column=5+1)
-smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="red", command=lambda: buttonClick(smallFieldsArray[1], 1)))
-smallFieldsArray[1].grid(row=0, column=4+1)
-smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="red", command=lambda: buttonClick(smallFieldsArray[2], 2)))
-smallFieldsArray[2].grid(row=0, column=3+1)
-smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="red", command=lambda: buttonClick(smallFieldsArray[3], 3)))
-smallFieldsArray[3].grid(row=0, column=2+1)
-smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="red", command=lambda: buttonClick(smallFieldsArray[4], 4)))
-smallFieldsArray[4].grid(row=0, column=1+1)
-smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="red", command=lambda: buttonClick(smallFieldsArray[5], 5)))
-smallFieldsArray[5].grid(row=0, column=0+1)
-smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(smallFieldsArray[6], 6)))
-smallFieldsArray[6].grid(row=2, column=4-3)
-smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(smallFieldsArray[7], 7)))
-smallFieldsArray[7].grid(row=2, column=5-3)
-smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(smallFieldsArray[8], 8)))
-smallFieldsArray[8].grid(row=2, column=6-3)
-smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(smallFieldsArray[9], 9)))
-smallFieldsArray[9].grid(row=2, column=7-3)
-smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(smallFieldsArray[10], 10)))
-smallFieldsArray[10].grid(row=2, column=8-3)
-smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(smallFieldsArray[11], 11)))
-smallFieldsArray[11].grid(row=2, column=9-3)
+def setupBoard():
+    smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="red", command=lambda: buttonClick(smallFieldsArray[0], 0)))
+    smallFieldsArray[0].grid(row=0, column=5+1)
+    smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="red", command=lambda: buttonClick(smallFieldsArray[1], 1)))
+    smallFieldsArray[1].grid(row=0, column=4+1)
+    smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="red", command=lambda: buttonClick(smallFieldsArray[2], 2)))
+    smallFieldsArray[2].grid(row=0, column=3+1)
+    smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="red", command=lambda: buttonClick(smallFieldsArray[3], 3)))
+    smallFieldsArray[3].grid(row=0, column=2+1)
+    smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="red", command=lambda: buttonClick(smallFieldsArray[4], 4)))
+    smallFieldsArray[4].grid(row=0, column=1+1)
+    smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="red", command=lambda: buttonClick(smallFieldsArray[5], 5)))
+    smallFieldsArray[5].grid(row=0, column=0+1)
+    smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(smallFieldsArray[6], 6)))
+    smallFieldsArray[6].grid(row=2, column=4-3)
+    smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(smallFieldsArray[7], 7)))
+    smallFieldsArray[7].grid(row=2, column=5-3)
+    smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(smallFieldsArray[8], 8)))
+    smallFieldsArray[8].grid(row=2, column=6-3)
+    smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(smallFieldsArray[9], 9)))
+    smallFieldsArray[9].grid(row=2, column=7-3)
+    smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(smallFieldsArray[10], 10)))
+    smallFieldsArray[10].grid(row=2, column=8-3)
+    smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(smallFieldsArray[11], 11)))
+    smallFieldsArray[11].grid(row=2, column=9-3)
+    for i in range(0, noOfBases):
+        basesArray.append(Button(root, text="0", height = 10, width = 10, fg="blue", state='disabled'))
+    basesArray[0].grid(row=0, column=0, rowspan=3)
+    basesArray[1].grid(row=0, column=7, rowspan=3)
 '''
 for i in range(0,noOfSmallFields):
     if (i < 4):
@@ -125,14 +165,10 @@ for i in range(0,noOfSmallFields):
         smallFieldsArray.append(Button(root, text="4", height = 5,  width = 10, fg="green", command=lambda: buttonClick(buttonIDs[i])))
         smallFieldsArray[i].grid(row=2, column=i-3)
 '''
-for i in range(0, noOfBases):
-    basesArray.append(Button(root, text="0", height = 10, width = 10, fg="blue", state='disabled'))
-
-basesArray[0].grid(row=0, column=0, rowspan=3)
-basesArray[1].grid(row=0, column=7, rowspan=3)
 
 
 
-
+## start the game
+setupBoard()
 disableButtons()
 root.mainloop()
